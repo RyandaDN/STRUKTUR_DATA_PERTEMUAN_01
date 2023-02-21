cd20d08c18858d66b751bb40d72dd95230ae40d6
RyandaDN <118288771+RyandaDN@users.noreply.github.com>
2023-02-21 08:54:59 +0700

    Delete STRUKTUR_DATA_PERTEMUAN_01 directory

--- STRUKTUR_DATA_PERTEMUAN_01/5.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int A=5,B=4;
-	do{
-		cout<<"B-- = "<<B--<<endl;
-		cout<<"(B|A) = "<<(B|A)<<endl;
-	}while((A|B)>=0);
-}
--- STRUKTUR_DATA_PERTEMUAN_01/7.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int A=5,B=4;
-	while((A&B)){
-		cout<<"--B = "<<--B<<endl;
-		cout<<"(B&A) = "<<(B&A)<<endl;
-	}
-}
