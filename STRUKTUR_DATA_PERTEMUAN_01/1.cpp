441c5fbcb23738af7500ffa92858af5136c3bb6b
RyandaDN <118288771+RyandaDN@users.noreply.github.com>
2023-02-21 08:55:53 +0700

    Add files via upload

---
+++ STRUKTUR_DATA_PERTEMUAN_01/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	i=10;
+	if(i=1){
+		cout<<"Ini Pernyataan dalam if"<<endl;
+	}
+	else
+	cout<<"Ini Pernyataan dalam else"<<endl;
+}
---
+++ STRUKTUR_DATA_PERTEMUAN_01/6.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int A=5,B=4;
+	do{
+		cout<<"--B = "<<--B<<endl;
+		cout<<"(B&A) = "<<(B&A)<<endl;
+	}while((A&B)>0);
+}
